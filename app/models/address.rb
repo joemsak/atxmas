c594b9c9a55b87adb5ab8b0f5d2219a43938ee54
Joe Sak <joe@joesak.com>
2012-11-09 13:17:43 -0600

    Public vs private addresses, public by default

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ActiveRecord::Base
+  default_scope where(:state => 'public')
+
+  scope :private_entries, where(:state => 'private')
+  scope :public_entries,  where(:state => 'public')
+
   attr_accessible :city, :country_id, :postal_code, :region_id, :state, :street, :user_id
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,5 +2,35 @@ require 'spec_no_rails_helper'
 require './app/models/address'
 
 describe Address do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "scopes" do
+    let!(:private) { Address.create!(:state => 'private') }
+
+    let!(:public) { Address.create!(:state => 'public') }
+
+    describe "default" do
+      it "includes public addresses" do
+        Address.all.should include(public)
+      end
+
+      it "does not include private addresses" do
+        Address.all.should_not include(private)
+      end
+    end
+
+    describe "private" do
+      it "includes private addresses" do
+        Address.private_entries.should include(private)
+      end
+    end
+
+    describe "public" do
+      it "includes public addresses" do
+        Address.public_entries.should include(public)
+      end
+
+      it "does not include private addresses" do
+        Address.public_entries.should_not include(private)
+      end
+    end
+  end
 end
