aa535cfa8067a54215d3c484b06362294a63a3d6
Joe Sak <joe@joesak.com>
2012-11-09 13:06:23 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atxmas::Application.config.secret_token = 'c6da8ef0b919eebd93c190ae21f00ec3c76f0f451b0d6ecd980c719dd44a2c75c59bc1b8f8abe31796a49b445f4294f558d6761fb56ede913172ca73009afc6d'
